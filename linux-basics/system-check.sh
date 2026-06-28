cee8ef830afbec52d3ae1cdfbc432e5b415cf936
Ola Eniola <olaeniola16@gmail.com>
2026-06-28 21:53:45 +0100

    Complete Linux foundation practice

---
+++ linux-basics/system-check.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+USER_NAME=$(whoami)
+CURRENT_DIR=$(pwd)
+TODAY=$(date)
+
+echo "Linux System Check"
+echo "User: $USER_NAME"
+echo "Current directory: $CURRENT_DIR"
+echo "Date: $TODAY"
